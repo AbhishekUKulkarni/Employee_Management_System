9e1afd0f7ee3b7ad75fbc3ac474ee35742966314
AbhishekUKulkarni <Abhishekkulkarni735@gmail.com>
2023-06-06 10:49:55 +0530

    Intial commit

---
+++ src/main/java/com/java/ems/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.java.ems.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.java.ems.model.Employee;
+import com.java.ems.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+	private EmployeeService employeeService ;
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService=employeeService;
+	}
+	@PostMapping()
+	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee)
+	{
+		return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+	}
+	@GetMapping()
+	public List<Employee> getAllEmployees(){
+		return employeeService.getAllEmployees();
+	}
+	@GetMapping("{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId){
+		return new ResponseEntity<>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
+	}
+	@PutMapping("{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id
+			,@RequestBody Employee employee){
+		return new ResponseEntity<>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+	}
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id){
+		employeeService.deleteEmployee(id);
+		return new ResponseEntity<>("Employee deleted successfully!.", HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/java/ems/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.java.ems.service;
+
+import java.util.List;
+
+import com.java.ems.model.Employee;
+
+public interface EmployeeService {
+
+	Employee saveEmployee(Employee employee);
+	List<Employee> getAllEmployees();
+	Employee getEmployeeById(long id);
+	Employee updateEmployee(Employee employee, long id);
+	void deleteEmployee(long id);
+}
